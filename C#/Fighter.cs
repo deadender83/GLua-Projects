2e75802b654d3c831b70c007ccf074ec4525867e
samuel cardo <97409997+deadender83@users.noreply.github.com>
2025-02-11 18:48:24 -0500

    Add files via upload

---
+++ C#/Fighter.cs
@@ -0,0 +1,70 @@
+﻿// Fighter.cs
+using System;
+
+namespace StreetFighterGame
+{
+    public class Fighter
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+
+        public Fighter(string name)
+        {
+            Name = name;
+            Health = 100;
+        }
+
+        public int PunchAttack()
+        {
+            return new Random().Next(1,7);
+        }
+
+        public int KickAttack()
+        {
+            return new Random().Next(5, 11);
+        }
+
+        public int SpeicalAttack1()
+        {
+            return new Random().Next(10, 15);
+        }
+
+        public int SpeicalAttack2()
+        {
+            return new Random().Next(10, 15);
+        }
+
+        public int SpeicalAttack3()
+        {
+            return new Random().Next(10, 15);
+        }
+
+        public void TakeDamage(int damage)
+        {
+            Health -= damage;
+            if (Health < 0) Health = 0;
+        }
+
+        public bool IsDefeated()
+        {
+            return Health == 0;
+        }
+
+        public int Player2Attacks()
+        {
+            int damage = 0;
+
+            if (PunchAttack() < 5)
+            {
+                damage = new Random().Next(1, 7);
+            }
+            else if (KickAttack() < 5)
+            {
+                damage = new Random().Next(5, 11);
+            }
+
+            return damage;
+        }
+
+    }
+}
---
+++ C#/Game.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace StreetFighterGame
+{
+    public partial class Game
+    {
+        private Fighter player1 { get; set; }
+        private Fighter player2 { get; set; }
+
+
+        public Fighter Player1
+        {
+            get { return player1; }
+        }
+
+        public Fighter Player2
+        {
+            get { return player2; }
+        }
+        public Game()
+        {
+            player1 = new Fighter("Player 1");
+            player2 = new Fighter("Player 2");
+        }
+
+        public void Punch()
+        {
+            int damage = player1.PunchAttack();
+            player2.TakeDamage(damage);
+            player2.Player2Attacks();
+        }
+
+        public void Kick()
+        {
+            int damage = player1.KickAttack();
+            player2.TakeDamage(damage);
+            player2.Player2Attacks();
+        }
+
+        public void SpeicalAttack1()
+        {
+            int damage = player1.SpeicalAttack1();
+            player2.TakeDamage(damage);
+        }
+
+        public void SpeicalAttack2()
+        {
+            int damage = player1.SpeicalAttack2();
+            player2.TakeDamage(damage);
+        }
+
+        public void SpeicalAttack3()
+        {
+            int damage = player1.SpeicalAttack3();
+            player2.TakeDamage(damage);
+        }
+
+        public void AttackPlayer2()
+        {
+            int damage = player2.PunchAttack();
+            player1.TakeDamage(damage);
+        }
+
+        public bool IsPlayer1Defeated()
+        {
+            return player1.IsDefeated();
+        }
+
+        public bool IsPlayer2Defeated()
+        {
+            return player2.IsDefeated();
+        }
+
+        public void ResetGame()
+        {
+            player1 = new Fighter("Player 1");
+            player2 = new Fighter("Player 2");
+        }
+        public void Player2Attacks()
+        {
+            int damage = player2.Player2Attacks();
+            player1.TakeDamage(damage);
+        }
+
+    }
+}
